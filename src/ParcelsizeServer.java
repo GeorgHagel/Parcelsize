cd4624643c79ec028e4ebb9302197661ca7f6a01
hagel <hagel@hagel-CELSIUS-H710-linux>
2018-04-18 12:01:57 +0200

    REST Service

--- src/ParcelsizeServer.java
+++
@@ -1,9 +0,0 @@
-
-public class ParcelsizeServer {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Parcelservice!");
-	}
-
-}
---
+++ src/service/Parcelsize.java
@@ -0,0 +1,7 @@
+package service;
+//package main.java;
+
+public enum Parcelsize {
+	XS,S,M,L,XL,UNDEFINED;
+	
+}
