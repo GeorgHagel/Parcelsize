1fd3f15bfe6afaa3837ac870cee8c56a5b4c1002
hagel <hagel@hagel-CELSIUS-H710-linux>
2018-07-11 14:15:21 +0200

    .

--- src/main/java/ParcelSizeService.java
+++ src/main/java/ParcelSizeService.java
@@ -23,7 +23,7 @@ public class ParcelSizeService {
 	
 	public ParcelSizeService() {
 		db = MySQLDatabaseHandler.getDatabaseHandler();
-		db.openConnection("jdbc:mysql://MySQLParcelsize:3306/ms_parcel_size?user=user&password=mysqlTest123,");
+		db.openConnection("jdbc:mysql://MySQLParcelsize:3306/ms_parcel_size?user=user&password=mysql");
 		
 		parcelSizeMap = new HashMap<Integer,Parcelsize>();
 		parcelSizeMap = db.getParcelSizeTable();
